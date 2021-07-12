cded8f82fb5b62e7f8b515d7667f51df1ccf65eb
svgmk07 <svgmk07@svgroup.hr>
2021-07-12 13:47:16 +0200

    Version change test

--- src/main/java/hr/kraljic/dockerdemo/TestController.java
+++ src/main/java/hr/kraljic/dockerdemo/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/")
     public String hello() {
-        return "Hello world!! :: " + DockerDemoApplication.number;
+        return "Hello world!! New version v2 :: " + DockerDemoApplication.number;
     }
 }
